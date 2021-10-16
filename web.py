edfc6009fe1abd86dfbec93d91df28af145073bd
Ingenious <76046349+Ingenious-c0der@users.noreply.github.com>
2021-10-16 16:53:25 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from webdriver_manager import chrome
 from webdriver_manager.chrome import ChromeDriverManager
 import re
+import os
 
-
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 class Web:
     async def get_google_predictions(request:str)->list:
         """Returns top list of predictions from google search
@@ -11,7 +17,7 @@ class Web:
             Returns : list [results] """
         req_list = request.split(" ")
         new_req = "+".join(req_list)
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
         results = []
         driver.get(fr"http://google.com/complete/search?output=toolbar&q={new_req}")
         content = driver.page_source
