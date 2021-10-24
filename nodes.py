2911fd00775b0ba74b241b91b8ee83f27f6e3571
Ingenious <76046349+Ingenious-c0der@users.noreply.github.com>
2021-10-24 16:17:15 +0530

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -105,7 +105,7 @@ class DrawNodes:
 
 
         v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12 = safe_list
-        print(safe_list.count(0), safe_list)
+        
         g = Graph(7,[[0,v2,v1,v3,0,0,0],
             [v2,0,v5,v4,0,0,0],
             [v1,v5,0,v6,v7,0,0],
